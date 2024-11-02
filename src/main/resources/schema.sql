ede23e877cf76ffc2183fc4841bba998b6cff9dc
maxprgita <mp.massimiliano@gmail.com>
2024-11-02 19:40:54 +0100

    aggiunta cartella src-test-resources e i 2 file sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,16 @@
 CREATE TABLE cliente (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255),
-    cognome VARCHAR(255),
-    email VARCHAR(255)
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    cognome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE prodotto (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    prezzo DECIMAL(10, 2) NOT NULL,
+    descrizione VARCHAR(255) NOT NULL,
+    genere VARCHAR(255) NOT NULL,
+    cliente_id BIGINT,
+    FOREIGN KEY (cliente_id) REFERENCES cliente(id)
+);
+
---
+++ src/test/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO cliente (nome, cognome, email) VALUES ('Mario', 'Rossi', 'mario.rossi@example.com');
+INSERT INTO cliente (nome, cognome, email) VALUES ('Luigi', 'Verdi', 'luigi.verdi@example.com');
+INSERT INTO cliente (nome, cognome, email) VALUES ('Carla', 'Bianchi', 'carla.bianchi@example.com');
+INSERT INTO cliente (nome, cognome, email) VALUES ('Giulia', 'Neri', 'giulia.neri@example.com');
+INSERT INTO cliente (nome, cognome, email) VALUES ('Paolo', 'Gialli', 'paolo.gialli@example.com');
+
+
+INSERT INTO prodotto (prezzo, descrizione, genere, cliente_id) VALUES (100.50, 'Laptop', 'Elettronica', 1);
+INSERT INTO prodotto (prezzo, descrizione, genere, cliente_id) VALUES (50.75, 'Smartphone', 'Elettronica', 2);
+INSERT INTO prodotto (prezzo, descrizione, genere, cliente_id) VALUES (25.99, 'Libro', 'Educazione', 3);
+INSERT INTO prodotto (prezzo, descrizione, genere, cliente_id) VALUES (10.99, 'Mouse', 'Elettronica', 1);
+INSERT INTO prodotto (prezzo, descrizione, genere, cliente_id) VALUES (15.00, 'Tastiera', 'Elettronica', 4);
+INSERT INTO prodotto (prezzo, descrizione, genere, cliente_id) VALUES (100.00, 'Cuffie', 'Audio', 5);
+INSERT INTO prodotto (prezzo, descrizione, genere, cliente_id) VALUES (30.00, 'Monitor', 'Elettronica', 2);
